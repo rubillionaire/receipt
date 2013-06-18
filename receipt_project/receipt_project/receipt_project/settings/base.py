69145643660ec2b236e4d0b1e5998839a8cd5cb8
rubillionaire <ruben@rubillionaire.com>
2013-06-18 14:19:22 -0400

    adding db config to base rather than just local

--- receipt_project/receipt_project/receipt_project/settings/base.py
+++ receipt_project/receipt_project/receipt_project/settings/base.py
@@ -43,8 +43,8 @@ MANAGERS = ADMINS
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.',
-        'NAME': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': normpath(join(DJANGO_ROOT, 'default.db')),
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
